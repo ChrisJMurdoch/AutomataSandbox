00ccf9e93978e1afa6a4a4b84e133e3dd3d52d2d
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2020-08-30 02:57:45 +0100

    Implemented PostgreSQL connection

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 
+<?php $db_connection = pg_connect("host=" . $_SERVER["DB_HOST"] . " dbname=" . $_SERVER["DB_NAME"] . " user=" . $_SERVER["DB_USER"] . " password=" . $_SERVER["DB_PASSWORD"] . ""); ?>
+<?php $users = pg_query($db_connection, "SELECT username FROM users"); ?>
 <?php include_once("home.html"); ?>

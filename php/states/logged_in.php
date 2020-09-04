362d5b2d54eb27348b41544eee626739ae2b79fa
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2020-09-04 03:23:12 +0100

    Added colouring to name

--- php/states/logged_in.php
+++ php/states/logged_in.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <html>
-    Logged in as <?php echo $_SESSION["username"]; ?>
+    Logged in as <span style="color: orange"><?php echo $_SESSION["username"]; ?></span>
     <form action="php/actions/log_out.php" method="post">
         <button type="submit" formmethod="post" name="action" value="log_out">Log Out</button>
     </form>
